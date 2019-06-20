5fd01e12ea8909a60a27f9c0f64186adfd6db4da
paulinhosouza1972@hotmail.com <paulinhosouza1972@hotmail.com>
2019-06-20 15:50:33 -0300

    Novas classes criadas

---
+++ QuickBuy.Repositorio/Repositorios/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using QuickBuy.Dominio.Entidade;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            var UsuarioRepositorio = new UsuarioRepositorio();
+            var produto = new Produto();
+            var usuario = new Usuario();
+
+            UsuarioRepositorio.Adicionar(usuario);
+        }
+    }
+}
