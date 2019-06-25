e1c5118632fea1bec6491948e3eb9c1290c1b274
paulinhosouza1972@hotmail.com <paulinhosouza1972@hotmail.com>
2019-06-25 17:58:12 -0300

    classes config

--- QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using QuickBuy.Dominio.Entidade;
 using QuickBuy.Dominio.ObjetoDeValor;
+using QuickBuy.Repositorio.Config;
 
 namespace QuickBuy.Repositorio.Contexto
 {
@@ -17,7 +18,17 @@ namespace QuickBuy.Repositorio.Contexto
         public QuickBuyContexto(DbContextOptions options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Classes de mapeamento aqui...
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemPedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new FormaPagamentoConfiguration());
 
+            base.OnModelCreating(modelBuilder);
+        }
     }
     }
 }
